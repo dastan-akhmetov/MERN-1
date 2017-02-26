a0a89ff69c8b7d257973729920c18997c5d5ae4b
Dastan Akhmetov <ddakhmet@gmail.com>
2017-02-26 12:16:59 +0600

    Updated Nav Component

--- app/src/Components/Menu/Nav.jsx
+++ app/src/Components/Menu/Nav.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import NavbarBrand from './Navbar-Brand.jsx';
 import Collapse from './Collapse.jsx';
 
-class Nav extends React.Component {
+export default class Nav extends React.Component {
     render() {
-        return  <div>
-                    <nav className="navbar navbar-default">
-                        <div className="container-fluid">
-                            <div className="navbar-header">
-                                <button type="button" 
-                                className="navbar-toggle collapsed" 
-                                data-toggle="collapse" 
-                                data-target="#bs-example-navbar-collapse-1">
-                                    <span className="sr-only">Toggle navigation</span>
-                                    <span className="icon-bar"></span>
-                                    <span className="icon-bar"></span>
-                                    <span className="icon-bar"></span>
-                                </button>
-                                <NavbarBrand href="#" name="MERN APP"/>
-                            </div>
-                            <Collapse/>
+        return  <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button type="button" 
+                            className="navbar-toggle collapsed" 
+                            data-toggle="collapse" 
+                            data-target="#bs-example-navbar-collapse-1">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <NavbarBrand href="#" name="MERN APP"/>
                         </div>
-                    </nav>
-                </div>
+                        <Collapse/>
+                    </div>
+                </nav>
     }
 }
-
-ReactDOM.render(<Nav/>, document.getElementById('app'));
